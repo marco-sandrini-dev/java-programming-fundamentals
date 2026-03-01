7dcec64c6acdb50582af1718b1c80691e8ca2219
marco-sandrini-dev <masandrinis@hotmail.com>
2026-03-01 16:43:49 -0300

    Update Beecrowd1040.java

--- beecrowd/Beecrowd1040.java
+++ beecrowd/Beecrowd1040.java
@@ -12,30 +12,31 @@ public class Beecrowd1040 {
         float n3 = sc.nextFloat();
         float n4 = sc.nextFloat();
 
-        float media = ((n1*2 + n2*3 + n3*4 + n4*1) / 10);
+        float average = ((n1*2 + n2*3 + n3*4 + n4*1) / 10);
 
-        System.out.println("Media: " + String.format("%.1f", media));
+        System.out.println("Average: " + String.format("%.1f", average));
 
-        if (media >= 7.0) {
-            System.out.println("Aluno aprovado.");
+        if (average >= 7.0) {
+            System.out.println("Student passed.");
         }
-        else if (media < 5.0) {
-            System.out.println("Aluno reprovado.");
+        else if (average < 5.0) {
+            System.out.println("Student failed.");
         }
         else {
-            System.out.println("Aluno em exame.");
-            float exame = sc.nextFloat();
-            System.out.println("Nota do exame: " + String.format("%.1f", exame));
-            float mediaFinal = (media + exame) / 2;
-            if (mediaFinal >= 5.0){
-                System.out.println("Aluno aprovado.");
+            System.out.println("Student in exam.");
+            float examGrade = sc.nextFloat();
+            System.out.println("Exam grade: " + String.format("%.1f", examGrade));
+            float finalAverage = (average + examGrade) / 2;
+            if (finalAverage >= 5.0){
+                System.out.println("Student passed.");
             }
             else{
-                System.out.println("Aluno reprovado.");
+                System.out.println("Student failed.");
             }
-            System.out.println("Media final: " + String.format("%.1f", mediaFinal));
+            System.out.println("Final average: " + String.format("%.1f", finalAverage));
         }
 
         sc.close();
     }
+
 }
